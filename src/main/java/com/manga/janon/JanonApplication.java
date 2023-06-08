bf43221ad2ddb0bfe7893685dc95b752ced0a0af
KX10Anonymous <kx10anonymous@gmail.com>
2023-06-08 15:33:10 +0200

    Init

---
+++ src/main/java/com/manga/janon/JanonApplication.java
@@ -0,0 +1,13 @@
+package com.manga.janon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manga/janon/JanonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manga.janon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
